0eed503a6d72d94594aad10d3c4ed3f9b97c35ea
Coder-Erro404 <kkhansare@gmail.com>
2021-07-10 13:05:26 +0530

    make it better

--- technico_app/static/js/main.js
+++ technico_app/static/js/main.js
@@ -1,226 +1,59 @@
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor");
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
 
-const textArray = ["Kanhaiya Khansare", "Web Developer","Software Tester"];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  //NAVIGATION
 
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-  	setTimeout(erase, newTextDelay);
-  }
-}
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (window.matchMedia('screen and (max-width: 768px)').matches){
 
-function erase() {
-	if (charIndex > 0) {
-    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } 
-  else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if(textArrayIndex>=textArray.length) textArrayIndex=0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
+  }else{
+    document.getElementById("navbar").style.top = "0";
+    document.getElementById("nav").style.display = "none";
+  }} else {
+  if (window.matchMedia('screen and (max-width: 768px)').matches){
 
-document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
-  if(textArray.length) setTimeout(type, newTextDelay + 250);
-});
-
-// navabar
-
-// Sticky navbar
-
-// navbar end
-// read more 
-$(document).ready(function() {
-  (function() {
-    var showChar = 300;
-    var ellipsestext = ".....";
-    $(".truncate").each(function() {
-      var content = $(this).html();
-      if (content.length > showChar) {
-        var c = content.substr(0, showChar);
-        var h = content;
-        var html =
-          '<div class="truncate-text" style="display:block">' +
-          c +
-          '<span class="moreellipses">' +
-          ellipsestext +
-          '&nbsp;&nbsp;<a href="" class="moreless more">  Read More</a></span></span></div><div class="truncate-text" style="display:none">' +
-          h +
-          '<a href="" class="moreless less">Less<br></a></span><br></div>';
-
-        $(this).html(html);
-      }
-    });
-
-    $(".moreless").click(function() {
-      var thisEl = $(this);
-      var cT = thisEl.closest(".truncate-text");
-      var tX = ".truncate-text";
-
-      if (thisEl.hasClass("less")) {
-        cT.prev(tX).toggle();
-        cT.slideToggle();
-      } else {
-        cT.toggle();
-        cT.next(tX).fadeToggle();
-      }
-      return false;
-    });
-    /* end iffe */
-  })();
-
-  /* end ready */
-});
-// read end
-
-
-
-
-// Gallery image hover
-$( ".img-wrapper" ).hover(
-  function() {
-    $(this).find(".img-overlay").animate({opacity: 1}, 600);
-  }, function() {
-    $(this).find(".img-overlay").animate({opacity: 0}, 600);
-  }
-);
-
-// Lightbox
-var $overlay = $('<div id="overlay"></div>');
-var $image = $("<img>");
-var $prevButton = $('<div id="prevButton"><i class="fa fa-chevron-left"></i></div>');
-var $nextButton = $('<div id="nextButton"><i class="fa fa-chevron-right"></i></div>');
-var $exitButton = $('<div id="exitButton"><i class="fa fa-times"></i></div>');
-
-// Add overlay
-$overlay.append($image).prepend($prevButton).append($nextButton).append($exitButton);
-$("#gallery").append($overlay);
-
-// Hide overlay on default
-$overlay.hide();
-
-// When an image is clicked
-$(".img-overlay").click(function(event) {
-  // Prevents default behavior
-  event.preventDefault();
-  // Adds href attribute to variable
-  var imageLocation = $(this).prev().attr("href");
-  // Add the image src to $image
-  $image.attr("src", imageLocation);
-  // Fade in the overlay
-  $overlay.fadeIn("slow");
-});
-
-// When the overlay is clicked
-$overlay.click(function() {
-  // Fade out the overlay
-  $(this).fadeOut("slow");
-});
-
-// When next button is clicked
-$nextButton.click(function(event) {
-  // Hide the current image
-  $("#overlay img").hide();
-  // Overlay image location
-  var $currentImgSrc = $("#overlay img").attr("src");
-  // Image with matching location of the overlay image
-  var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
-  // Finds the next image
-  var $nextImg = $($currentImg.closest(".image").next().find("img"));
-  // All of the images in the gallery
-  var $images = $("#image-gallery img");
-  // If there is a next image
-  if ($nextImg.length > 0) { 
-    // Fade in the next image
-    $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
-  } else {
-    // Otherwise fade in the first image
-    $("#overlay img").attr("src", $($images[0]).attr("src")).fadeIn(800);
-  }
-  // Prevents overlay from being hidden
-  event.stopPropagation();
-});
-
-// When previous button is clicked
-$prevButton.click(function(event) {
-  // Hide the current image
-  $("#overlay img").hide();
-  // Overlay image location
-  var $currentImgSrc = $("#overlay img").attr("src");
-  // Image with matching location of the overlay image
-  var $currentImg = $('#image-gallery img[src="' + $currentImgSrc + '"]');
-  // Finds the next image
-  var $nextImg = $($currentImg.closest(".image").prev().find("img"));
-  // Fade in the next image
-  $("#overlay img").attr("src", $nextImg.attr("src")).fadeIn(800);
-  // Prevents overlay from being hidden
-  event.stopPropagation();
-});
-
-// When the exit button is clicked
-$exitButton.click(function() {
-  // Fade out the overlay
-  $("#overlay").fadeOut("slow");
-});
-
-// gallery end
-
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+  }else{
+    document.getElementById("navbar").style.top = "-85px";
+    document.getElementById("nav").style.display = "block";
     }
   }
-  element.className = arr1.join(" ");
 }
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+var count=0;
+function opennav(){
+if(count==0){
+document.getElementById("nav").style.height="240px";
+document.getElementById("icon").style.color="#3399ff";
+count++;
+}else{
+document.getElementById("nav").style.height="45px";
+document.getElementById("icon").style.color="#000";
+count=0;
 }
-
-
+}
